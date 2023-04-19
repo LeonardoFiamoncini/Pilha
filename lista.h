d91d7a6851abfbe152fb277f1574eeaff64adb9f
LeonardoFiamoncini <leonardosfiamoncini@gmail.com>
2023-04-19 15:36:33 -0300

    Adição de comentário no formato Doxygen em lista.h

--- lista.h
+++ lista.h
@@ -5,6 +5,11 @@
 #include <stdlib.h>
 #include <locale.h>
 
+/**
+* \author Leonardo de Souza Fiamoncini
+* \since 19/04/2023
+* \brief Struct representing a data structure known as linked list 
+**/
 typedef struct noLista {
     float info;
     struct noLista *prox;
